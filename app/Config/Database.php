d101fefd3f623669a3a6e25c38f16a889dfaa0a5
Alvaro <alvarorent2001@gmail.com>
2023-03-18 21:04:21 -0600

    Mi parte

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'rody1999',
+        'password' => '',
         'database' => 'db_clinica',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
